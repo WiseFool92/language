7ad1eff21a8a095e6900af0face2fe6569221b76
Nathan Watkins Hoagland <watkins92@gmail.com>
2020-03-27 14:06:28 -0700

    js array w/for else if statement

--- js/scripts.js
+++ js/scripts.js
@@ -6,5 +6,24 @@ $(document).ready(function(){
     var question3 = $("input:radio[name=q3]:checked").val();
     var question4 = $("input:radio[name=q4]:checked").val();
     var question5 = $("input:radio[name=q5]:checked").val();
+    var aResults = 0;
+    var bResults = 0;
+    var cResults = 0;
+
+    var outcomesArray = [question1,question2,question3,question4,question5]
+
+    for (let i = 0; i > 5; i++) {
+      if (outcomesArray[i] === "a") {
+        aResults += 1;
+      }
+      else if (outcomesArray[i] === "b") {
+        bResults += 1;
+      }
+      else if (outcomesArray[i] === "c") {
+        cResults += 1;
+      }
+    }
+
+    var initialWinner = "a";
   }
 }
